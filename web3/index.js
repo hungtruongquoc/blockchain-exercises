d9543c76d2e713e0c7e75e541e9a8af347f74c7a
Hung Truong <hungtruongquoc@gmail.com>
2024-05-12 00:01:26 -0500

    20240511: Added new contract and truffle boilerplate

--- web3/index.js
+++ web3/index.js
@@ -1,6 +1,7 @@
 const {Web3, utils} = require('web3')
-const mainNetUrl = 'https://mainnet.infura.io/v3/75d8633578a941c8af149434ce47842e'
-const web3Client = new Web3(mainNetUrl)
+// const mainNetUrl = 'https://mainnet.infura.io/v3/75d8633578a941c8af149434ce47842e'
+const localNetUrl= 'http://localhost:8545'
+const web3Client = new Web3(localNetUrl)
 const address = '0x5C40e6813c86A9332e42cF30c1faA5541830b79d'
 
 const getBalance = async (address) => {
@@ -8,6 +9,9 @@ const getBalance = async (address) => {
     console.log(utils.fromWei(response, 'ether'))
     const transactionCount = await web3Client.eth.getTransactionCount(address)
     console.log(transactionCount)
+    const chainId = await web3Client.eth.getChainId()
+    // To answer the title directly, the chain_id of ganache is 0x539 (or 1337).
+    console.log(chainId)
 }
 
 getBalance(address)
